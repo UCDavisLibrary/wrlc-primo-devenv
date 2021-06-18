baf10fa4ed3b9ff84cb1d1ff1fa1210dc59a0094
spelkey-ucd <spelkey@ucdavis.edu>
2021-06-18 10:43:53 -0400

    remove central package

--- views/01UCD_INST-UCD/js/custom.js
+++ views/01UCD_INST-UCD/js/custom.js
@@ -216,6 +216,77 @@ app.component('prmSearchResultAvailabilityLineAfter', {
   controller: 'prmSearchResultAvailabilityLineAfterController',
   template: '<hathi-trust-availability hide-online="true" hide-if-journal="false" ignore-copyright="true"></hathi-trust-availability><ga-hathi-load event-sent="{{$ctrl.gaHathiLoad()}}"></ga-hathi-load>'
 });
+angular.module('externalSearch', []).value('searchTargets', []).value('externalSearchText', "").component('prmFacetAfter', {
+  bindings: { parentCtrl: '<' },
+  controller: ['externalSearch', '$scope', function (externalSearch, $scope) {
+    this.$onInit = function () {
+      externalSearch.setController(this.parentCtrl);
+      externalSearch.addExtSearch();
+      $scope.$watch('$ctrl.parentCtrl.facets', function () {
+        externalSearch.addExtSearch();
+      });
+    };
+  }]
+}).component('prmPageNavMenuAfter', {
+  controller: ['externalSearch', function (externalSearch) {
+    this.$onInit = function () {
+      if (externalSearch.getController()) externalSearch.addExtSearch();
+    };
+  }]
+}).component('prmFacetExactAfter', {
+  bindings: { parentCtrl: '<' },
+  template: '<div ng-if="name === \'External Search\'">\
+          <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">\
+              <div class="section-content animate-max-height-variable">\
+                  <div ng-if="introText" id="external-search-intro">{{introText}}</div>\
+                  <div class="md-chips md-chips-wrap">\
+                      <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">\
+                          <div class="md-chip-content layout-row" role="button" tabindex="0">\
+                              <strong dir="auto" title="{{ target.name }}">\
+                                  <a ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">\
+                                      <img ng-src="{{ target.img }}" width="22" height="22" alt="{{ target.alt }}" style="vertical-align:middle;"> {{ target.name }}\
+                                  </a>\
+                              </strong>\
+                          </div>\
+                      </div>\
+                  </div>\
+              </div>\
+          </div>\
+      </div>',
+  controller: ['$scope', '$location', 'searchTargets', 'externalSearchText', function ($scope, $location, searchTargets, externalSearchText) {
+    this.$onInit = function () {
+      $scope.name = this.parentCtrl.facetGroup.name;
+      $scope.targets = searchTargets;
+      $scope.introText = externalSearchText;
+      var query = $location.search().query;
+      var filter = $location.search().pfilter;
+      $scope.queries = Object.prototype.toString.call(query) === '[object Array]' ? query : query ? [query] : false;
+      $scope.filters = Object.prototype.toString.call(filter) === '[object Array]' ? filter : filter ? [filter] : false;
+    };
+  }]
+}).factory('externalSearch', function () {
+  return {
+    getController: function getController() {
+      return this.prmFacetCtrl || false;
+    },
+    setController: function setController(controller) {
+      this.prmFacetCtrl = controller;
+    },
+    addExtSearch: function addExtSearch(ctrl) {
+      if (!ctrl) ctrl = this.prmFacetCtrl;
+      if (ctrl.facets.length < 1 || ctrl.facets[0].name !== 'External Search') {
+        ctrl.facets.unshift({
+          name: 'External Search',
+          displayedType: 'exact',
+          limitCount: 0,
+          facetGroupCollapsed: false,
+          values: undefined
+        });
+      }
+    }
+  };
+});
+
 angular.module('hathiTrustAvailability', []).constant('hathiTrustBaseUrl', 'https://catalog.hathitrust.org/api/volumes/brief/json/').config(['$sceDelegateProvider', 'hathiTrustBaseUrl', function ($sceDelegateProvider, hathiTrustBaseUrl) {
   var urlWhitelist = $sceDelegateProvider.resourceUrlWhitelist();
   urlWhitelist.push(hathiTrustBaseUrl + '**');
@@ -378,76 +449,5 @@ angular.module('hathiTrustAvailability', []).constant('hathiTrustBaseUrl', 'http
                   <prm-icon external-link="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>\
                 </a>\
               </span>'
-});
-
-angular.module('externalSearch', []).value('searchTargets', []).value('externalSearchText', "").component('prmFacetAfter', {
-  bindings: { parentCtrl: '<' },
-  controller: ['externalSearch', '$scope', function (externalSearch, $scope) {
-    this.$onInit = function () {
-      externalSearch.setController(this.parentCtrl);
-      externalSearch.addExtSearch();
-      $scope.$watch('$ctrl.parentCtrl.facets', function () {
-        externalSearch.addExtSearch();
-      });
-    };
-  }]
-}).component('prmPageNavMenuAfter', {
-  controller: ['externalSearch', function (externalSearch) {
-    this.$onInit = function () {
-      if (externalSearch.getController()) externalSearch.addExtSearch();
-    };
-  }]
-}).component('prmFacetExactAfter', {
-  bindings: { parentCtrl: '<' },
-  template: '<div ng-if="name === \'External Search\'">\
-          <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">\
-              <div class="section-content animate-max-height-variable">\
-                  <div ng-if="introText" id="external-search-intro">{{introText}}</div>\
-                  <div class="md-chips md-chips-wrap">\
-                      <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">\
-                          <div class="md-chip-content layout-row" role="button" tabindex="0">\
-                              <strong dir="auto" title="{{ target.name }}">\
-                                  <a ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">\
-                                      <img ng-src="{{ target.img }}" width="22" height="22" alt="{{ target.alt }}" style="vertical-align:middle;"> {{ target.name }}\
-                                  </a>\
-                              </strong>\
-                          </div>\
-                      </div>\
-                  </div>\
-              </div>\
-          </div>\
-      </div>',
-  controller: ['$scope', '$location', 'searchTargets', 'externalSearchText', function ($scope, $location, searchTargets, externalSearchText) {
-    this.$onInit = function () {
-      $scope.name = this.parentCtrl.facetGroup.name;
-      $scope.targets = searchTargets;
-      $scope.introText = externalSearchText;
-      var query = $location.search().query;
-      var filter = $location.search().pfilter;
-      $scope.queries = Object.prototype.toString.call(query) === '[object Array]' ? query : query ? [query] : false;
-      $scope.filters = Object.prototype.toString.call(filter) === '[object Array]' ? filter : filter ? [filter] : false;
-    };
-  }]
-}).factory('externalSearch', function () {
-  return {
-    getController: function getController() {
-      return this.prmFacetCtrl || false;
-    },
-    setController: function setController(controller) {
-      this.prmFacetCtrl = controller;
-    },
-    addExtSearch: function addExtSearch(ctrl) {
-      if (!ctrl) ctrl = this.prmFacetCtrl;
-      if (ctrl.facets.length < 1 || ctrl.facets[0].name !== 'External Search') {
-        ctrl.facets.unshift({
-          name: 'External Search',
-          displayedType: 'exact',
-          limitCount: 0,
-          facetGroupCollapsed: false,
-          values: undefined
-        });
-      }
-    }
-  };
 });
 })();
